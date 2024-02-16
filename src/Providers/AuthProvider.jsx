6de15e2d25f40f7396cdf9543f9e935c2fc95e29
khalilsikder <khalil.sikder03@gmail.com>
2024-02-17 02:09:15 +0600

    use axios

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { createContext } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword,signInWithPopup,signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import axios from "axios";
 export const AuthContext = createContext(null)
 const auth = getAuth(app);
 
@@ -44,6 +45,15 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
             console.log('current user', currentUser);
+
+            // get and set token
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt',{email:currentUser.email})
+                .then(data =>{
+                    localStorage.setItem('access.token',data.data.token)
+                })
+            }
+
             setLoading(false)
         })
         return () => {
